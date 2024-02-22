d61ec13eaaa94aa1e6446b0a08b0ef6e70c82dcd
Evgeny Mizulin <evgeny.mizulin@gmail.com>
2024-02-22 16:17:48 +0300

    export

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,6 @@
+/* components */
 export { Skeleton } from './components/common/Skeleton';
 export { InvoiceboxLoader } from './components/common/InvoiceboxLoader';
+
+/* palette */
+export { PaletteProvider } from './palette';
