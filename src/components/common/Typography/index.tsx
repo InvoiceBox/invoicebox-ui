84a25c6b2f2db31d47be63323c50ff9aa65fe8f1
Evgeny Mizulin <evgeny.mizulin@gmail.com>
2024-03-05 17:33:46 +0300

    fonts via effect

--- src/components/common/Typography/index.tsx
+++ src/components/common/Typography/index.tsx
@@ -1,7 +1,6 @@
-import React, { ButtonHTMLAttributes, FC, HTMLAttributes } from 'react';
+import React, { ButtonHTMLAttributes, FC, HTMLAttributes, useEffect } from 'react';
 import * as S from './styles';
 import { typography } from './typography';
-import { Helmet } from 'react-helmet';
 
 type TDivProps = { element?: 'div' } & HTMLAttributes<HTMLDivElement>;
 type TParagraphProps = { element: 'p' } & HTMLAttributes<HTMLParagraphElement>;
@@ -20,16 +19,28 @@ export const Typography: FC<TProps> = ({ variant, element = 'div', ...rest }) =>
 };
 
 export const TypographyGlobalStyle: FC = () => {
-    return (
-        <Helmet>
-            <link rel="preconnect" href="https://fonts.googleapis.com" />
-            {/* eslint-disable-next-line @typescript-eslint/ban-ts-comment */}
-            {/* @ts-ignore */}
-            <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
-            <link
-                href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap"
-                rel="stylesheet"
-            />
-        </Helmet>
-    );
+    useEffect(() => {
+        const primaryLink = document.createElement('link');
+        primaryLink.rel = 'preconnect';
+        primaryLink.href = 'https://fonts.googleapis.com';
+
+        const secondaryLink = document.createElement('link');
+        secondaryLink.rel = 'preconnect';
+        secondaryLink.href = 'https://fonts.gstatic.com';
+        secondaryLink.crossOrigin = '';
+
+        const thirdLink = document.createElement('link');
+        thirdLink.rel = 'stylesheet';
+        thirdLink.href = 'https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap';
+
+        const links = [primaryLink, secondaryLink, thirdLink];
+
+        links.forEach((link) => document.head.appendChild(link));
+
+        return () => {
+            links.forEach((link) => document.head.removeChild(link));
+        };
+    }, []);
+
+    return null;
 };
