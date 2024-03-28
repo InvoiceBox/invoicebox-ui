cd9bba8bb996909903babcc6b9047de64ea55334
Evgeny Mizulin <evgeny.mizulin@gmail.com>
2024-03-28 14:33:17 +0300

    date-input-docs

---
+++ src/components/form/DateInput/docs.stories.tsx
@@ -0,0 +1,26 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import Markdown from 'markdown-to-jsx';
+import { DateInput } from '.';
+import { Typography } from '../../common/Typography';
+
+const meta = {
+    title: 'form/DateInput',
+} satisfies Meta<typeof DateInput>;
+
+export default meta;
+
+export const Installation: StoryObj = {
+    render: () => (
+        <Typography variant="bodyL">
+            <Markdown>
+                {`
+### Установка
+
+DateInput использует компонент Calendar.
+Calendar должен быть установлен. 
+[Подробнее.](/?path=/story/common-calendar--installation)
+`}
+            </Markdown>
+        </Typography>
+    ),
+};
