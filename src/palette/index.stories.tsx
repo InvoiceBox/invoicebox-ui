625e30fcf553d8eb3752cfa88902c81b2c31cc6c
Evgeny Mizulin <evgeny.mizulin@gmail.com>
2024-03-29 13:21:01 +0300

    docs

--- src/palette/index.stories.tsx
+++ src/palette/index.stories.tsx
@@ -1,7 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import { Skeleton } from '../components/common/Skeleton';
 import { PaletteProvider } from './provider';
-import Markdown from 'markdown-to-jsx';
 import { FC, ReactNode } from 'react';
 import { Typography } from '../components/common/Typography';
 
@@ -18,157 +17,157 @@ export default meta;
 export const Docs: StoryObj = {
     render: () => (
         <Typography variant="bodyL">
-            <Markdown>
-                {`
-# Система цветов
-
-### Поведение по умолчанию
-
-Можно использовать компоненты без определения цветов, тогда будут использованы цвета по умолчанию.
-
-\`\`\`ts
-<Skeleton />
-\`\`\`
-`}
-            </Markdown>
-            <Example>
-                <Skeleton />
-            </Example>
-            <Markdown>
-                {`
-### Переопределение цветов на уровне вызова компонента
-
-Можно переопределить цвета на уровне вызова компонента, для этого используйте сойство \`palette\`.
-
-\`\`\`ts
-<Skeleton palette={{ bg: '#c9c', flicker: '#5a5' }} />
-\`\`\`
-`}
-            </Markdown>
-            <Example>
-                <Skeleton palette={{ bg: '#c9c', flicker: '#5a5' }} />
-            </Example>
-            <Markdown>
-                {`
-### Переопределение цветов конкретного компонента
-
-Можно переопределить поведение по умолчанию конкретного компонента. Для этого используйте \`PaletteProvider\`
-и его свойство соответствующее компоненту.
-
-\`\`\`ts
-<PaletteProvider skeleton={{ bg: '#39c', flicker: '#5cc' }}>
-    <Skeleton />
-</PaletteProvider>
-\`\`\`
-`}
-            </Markdown>
-            <Example>
-                <PaletteProvider skeleton={{ bg: '#39c', flicker: '#5cc' }}>
+            <h1>Система цветов</h1>
+            <div>
+                <h3>Поведение по умолчанию</h3>
+                <p>
+                    Можно использовать компоненты без определения цветов, тогда будут использованы цвета по
+                    умолчанию.
+                </p>
+                <pre>
+                    <code>{'<Skeleton />'}</code>
+                </pre>
+                <Example>
                     <Skeleton />
-                </PaletteProvider>
-            </Example>
-            <Markdown>
-                {`
-### Переопределение абстрактных цветов
-
-Все цвета компонентов создаются из абстрактных цветов. Переопределяя абстрактные цвета, можно изменить 
-цветовую палитру всех компонентов разом. Это может быть полезно для темирования.
-Для переопределения абстрактных цветов используйте \`PaletteProvider\` и его свойство \`abstract\`.
-
-\`\`\`ts
-<PaletteProvider abstract={{ neutralUsual: '#c93', neutralLight: '#cc5' }}>
-    <Skeleton />
-</PaletteProvider>
-\`\`\`
-`}
-            </Markdown>
-            <Example>
-                <PaletteProvider abstract={{ neutralUsual: '#c93', neutralLight: '#cc5' }}>
-                    <Skeleton />
-                </PaletteProvider>
-            </Example>
-            <Markdown>
-                {`
-### Приоритет цветов
-
-Самый высокий приоритет имеют цвета на уровне вызова компонента. Затем цвета по умолчанию, 
-назначенные при помощи \`PaletteProvider\` и соответствующего свойства. Затем абстрактные цвета, 
-назначенные при помощи \`PaletteProvider\` и свойства \`abstract\`.
-
-\`\`\`ts
-<PaletteProvider abstract={{ neutralUsual: 'red' }} skeleton={{ bg: 'green' }}>
-    <Skeleton palette={{ bg: 'blue' }}/>
-</PaletteProvider>
-\`\`\`
-`}
-            </Markdown>
-            <Example>
-                <PaletteProvider abstract={{ neutralUsual: 'red' }} skeleton={{ bg: 'green' }}>
-                    <Skeleton palette={{ bg: 'blue' }} />
-                </PaletteProvider>
-            </Example>
-            <Markdown>
-                {`
-\`\`\`ts
-<PaletteProvider abstract={{ neutralUsual: 'red' }} skeleton={{ bg: 'green' }}>
-    <Skeleton />
-</PaletteProvider>
-\`\`\`
-`}
-            </Markdown>
-            <Example>
-                <PaletteProvider abstract={{ neutralUsual: 'red' }} skeleton={{ bg: 'green' }}>
-                    <Skeleton />
-                </PaletteProvider>
-            </Example>
-            <Markdown>
-                {`
-\`\`\`ts
-<PaletteProvider abstract={{ neutralUsual: 'red' }}>
-    <Skeleton />
-</PaletteProvider>
-\`\`\`
-`}
-            </Markdown>
-            <Example>
-                <PaletteProvider abstract={{ neutralUsual: 'red' }}>
-                    <Skeleton />
-                </PaletteProvider>
-            </Example>
-            <Markdown>
-                {`
-### Частичное и полное переопределение цветов
-
-На любом уровне можно переопределить любое количество цветов.
-
-\`\`\`ts
-<Skeleton palette={{ bg: 'pink' }}/>
-\`\`\`
-`}
-            </Markdown>
-            <Example>
-                <Skeleton palette={{ bg: 'pink' }} />
-            </Example>
-            <Markdown>
-                {`
-\`\`\`ts
-<Skeleton palette={{ flicker: 'brown' }}/>
-\`\`\`
-`}
-            </Markdown>
-            <Example>
-                <Skeleton palette={{ flicker: 'brown' }} />
-            </Example>
-            <Markdown>
-                {`
-\`\`\`ts
-<Skeleton palette={{ bg: 'pink', flicker: 'brown' }}/>
-\`\`\`
-`}
-            </Markdown>
-            <Example>
-                <Skeleton palette={{ bg: 'pink', flicker: 'brown' }} />
-            </Example>
+                </Example>
+            </div>
+            <div>
+                <h3>Переопределение цветов на уровне вызова компонента</h3>
+                <p>
+                    Можно переопределить цвета на уровне вызова компонента, для этого используйте свойство{' '}
+                    <code>palette</code>.
+                </p>
+                <pre>
+                    <code>{"<Skeleton palette={{ bg: '#c9c', flicker: '#5a5' }} />"}</code>
+                </pre>
+                <Example>
+                    <Skeleton palette={{ bg: '#c9c', flicker: '#5a5' }} />
+                </Example>
+                <p>
+                    <i>
+                        Если у компонента нет свойства <code>palette</code>, а вам оно требуется, создайте{' '}
+                        <a href="https://github.com/InvoiceBox/invoicebox-ui/issues">issue</a>. Или добавьте
+                        самостоятельно и откройте pull request, используя в качестве примера, компонент{' '}
+                        <code>Skeleton</code>.
+                    </i>
+                </p>
+            </div>
+            <div>
+                <h3>Переопределение цветов конкретного компонента</h3>
+                <p>
+                    Можно переопределить поведение по умолчанию конкретного компонента. Для этого используйте{' '}
+                    <code>PaletteProvider</code> и его свойство соответствующее компоненту.
+                </p>
+                <pre>
+                    <code>
+                        {`<PaletteProvider skeleton={{ bg: '#39c', flicker: '#5cc' }}>\n`}
+                        {`    <Skeleton />\n`}
+                        {`</PaletteProvider>`}
+                    </code>
+                </pre>
+                <Example>
+                    <PaletteProvider skeleton={{ bg: '#39c', flicker: '#5cc' }}>
+                        <Skeleton />
+                    </PaletteProvider>
+                </Example>
+                <p>
+                    <i>
+                        Если у <code>PaletteProvider</code> нет свойства соответствующего компоненту, а вам
+                        оно требуется, создайте{' '}
+                        <a href="https://github.com/InvoiceBox/invoicebox-ui/issues">issue</a>. Или добавьте
+                        самостоятельно и откройте pull request, используя в качестве примера, компонент{' '}
+                        <code>Skeleton</code>.
+                    </i>
+                </p>
+            </div>
+            <div>
+                <h3>Переопределение абстрактных цветов</h3>
+                <p>
+                    Все цвета компонентов создаются из абстрактных цветов. Переопределяя абстрактные цвета,
+                    можно изменить цветовую палитру всех компонентов разом. Это может быть полезно для
+                    темирования. Для переопределения абстрактных цветов используйте{' '}
+                    <code>PaletteProvider</code> и его свойство <code>abstract</code>.
+                </p>
+                <pre>
+                    <code>
+                        {`<PaletteProvider abstract={{ neutralUsual: '#c93', neutralLight: '#cc5' }}>\n`}
+                        {`    <Skeleton />\n`}
+                        {`</PaletteProvider>`}
+                    </code>
+                </pre>
+                <Example>
+                    <PaletteProvider abstract={{ neutralUsual: '#c93', neutralLight: '#cc5' }}>
+                        <Skeleton />
+                    </PaletteProvider>
+                </Example>
+            </div>
+            <div>
+                <h3>Приоритет цветов</h3>
+                <p>
+                    Самый высокий приоритет имеют цвета на уровне вызова компонента. Затем цвета по умолчанию,
+                    назначенные при помощи <code>PaletteProvider</code> и соответствующего свойства. Затем
+                    абстрактные цвета, назначенные при помощи <code>PaletteProvider</code> и свойства{' '}
+                    <code>abstract</code>.
+                </p>
+                <pre>
+                    <code>
+                        {`<PaletteProvider abstract={{ neutralUsual: 'red' }} skeleton={{ bg: 'green' }}>\n`}
+                        {`    <Skeleton palette={{ bg: 'blue' }}/>\n`}
+                        {`</PaletteProvider>`}
+                    </code>
+                </pre>
+                <Example>
+                    <PaletteProvider abstract={{ neutralUsual: 'red' }} skeleton={{ bg: 'green' }}>
+                        <Skeleton palette={{ bg: 'blue' }} />
+                    </PaletteProvider>
+                </Example>
+                <pre>
+                    <code>
+                        {`<PaletteProvider abstract={{ neutralUsual: 'red' }} skeleton={{ bg: 'green' }}>\n`}
+                        {`    <Skeleton />\n`}
+                        {`</PaletteProvider>`}
+                    </code>
+                </pre>
+                <Example>
+                    <PaletteProvider abstract={{ neutralUsual: 'red' }} skeleton={{ bg: 'green' }}>
+                        <Skeleton />
+                    </PaletteProvider>
+                </Example>
+                <pre>
+                    <code>
+                        {`<PaletteProvider abstract={{ neutralUsual: 'red' }}>\n`}
+                        {`    <Skeleton />\n`}
+                        {`</PaletteProvider>`}
+                    </code>
+                </pre>
+                <Example>
+                    <PaletteProvider abstract={{ neutralUsual: 'red' }}>
+                        <Skeleton />
+                    </PaletteProvider>
+                </Example>
+            </div>
+            <div>
+                <h3>Частичное и полное переопределение цветов</h3>
+                <p>На любом уровне можно переопределить любое количество цветов.</p>
+                <pre>
+                    <code>{`<Skeleton palette={{ bg: 'pink' }}/>`}</code>
+                </pre>
+                <Example>
+                    <Skeleton palette={{ bg: 'pink' }} />
+                </Example>
+                <pre>
+                    <code>{`<Skeleton palette={{ flicker: 'brown' }}/>`}</code>
+                </pre>
+                <Example>
+                    <Skeleton palette={{ flicker: 'brown' }} />
+                </Example>
+                <pre>
+                    <code>{`<Skeleton palette={{ bg: 'pink', flicker: 'brown' }}/>`}</code>
+                </pre>
+                <Example>
+                    <Skeleton palette={{ bg: 'pink', flicker: 'brown' }} />
+                </Example>
+            </div>
         </Typography>
     ),
 };
