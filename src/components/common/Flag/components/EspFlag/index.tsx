5249c88d535f49b41c97623036c7f7b94021747c
Dmitry Sorokin <irondimk@gmail.com>
2024-04-03 20:05:32 +0700

    flag fix

--- src/components/common/Flag/components/EspFlag/index.tsx
+++ src/components/common/Flag/components/EspFlag/index.tsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 export const EspFlag = () => (
     <svg width="100%" height="100%" viewBox="0 0 22 16" fill="none" xmlns="http://www.w3.org/2000/svg">
         <g id="artwork">
