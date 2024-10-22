6f7322a132e918328dacf7793939bb110c8a14e7
Dmitry Sorokin <irondimk@gmail.com>
2024-10-22 18:35:12 +0700

    Revert "remove portal"
    This reverts commit af3dcf234828378fe453840fd9cf6592e880fb1e.

--- src/components/_helpers/StoryOneFieldForm/index.tsx
+++ src/components/_helpers/StoryOneFieldForm/index.tsx
@@ -2,6 +2,7 @@ import React, { FC, ReactNode } from 'react';
 import { action } from '@storybook/addon-actions';
 import { Resolver, useForm } from 'react-hook-form';
 import { FormProvider } from 'react-hook-form';
+import { Portal } from '../../common/Portal';
 import { DevTool } from '@hookform/devtools';
 
 export const FIELD_NAME = 'field';
@@ -31,7 +32,10 @@ export const StoryOneFieldForm: FC<TProps> = ({ children, initialValue, resolver
             <form onSubmit={handleSubmit(submit)}>
                 {children}
                 <button type="submit">Отправить</button>
-                <DevTool control={methods.control} placement="top-right" />
+
+                <Portal>
+                    <DevTool control={methods.control} placement="top-right" />
+                </Portal>
             </form>
         </FormProvider>
     );
