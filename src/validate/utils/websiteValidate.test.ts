f7af1efb9426ff1e832b0cd34da77050a42886fe
Dmitry Sorokin <irondimk@gmail.com>
2025-09-16 14:26:52 +0700

    add query params

--- src/validate/utils/websiteValidate.test.ts
+++ src/validate/utils/websiteValidate.test.ts
@@ -11,6 +11,9 @@ describe('Website validate', () => {
         expect(validateAllFunc('https://www.abc.com')).toBe(true);
         expect(validateAllFunc('https://дом.рф')).toBe(true);
         expect(validateAllFunc('дом.рф')).toBe(true);
+        expect(validateAllFunc('http://abc.com/anyhere')).toBe(true);
+        expect(validateAllFunc('http://abc.com/anyhere/anyhere')).toBe(true);
+        expect(validateAllFunc('https://abc.com/anyhere/anyhere?any')).toBe(true);
 
         expect(validateAllFunc('https://www')).toBe(WEBSITE_WRONG_MESSAGE);
         expect(validateAllFunc('www')).toBe(WEBSITE_WRONG_MESSAGE);
@@ -23,12 +26,15 @@ describe('Website validate', () => {
         expect(validateHttpsFunc('https://abc.com')).toBe(true);
         expect(validateHttpsFunc('https://www.abc.com')).toBe(true);
         expect(validateHttpsFunc('https://дом.рф')).toBe(true);
+        expect(validateAllFunc('https://abc.com/anyhere')).toBe(true);
+        expect(validateAllFunc('https://abc.com/anyhere/anyhere')).toBe(true);
+        expect(validateAllFunc('https://abc.com/anyhere/anyhere?any')).toBe(true);
 
         expect(validateHttpsFunc('дом.рф')).toBe(WEBSITE_WRONG_MESSAGE);
         expect(validateHttpsFunc('http://www.abc.com')).toBe(WEBSITE_WRONG_MESSAGE);
         expect(validateHttpsFunc('abc.com')).toBe(WEBSITE_WRONG_MESSAGE);
         expect(validateHttpsFunc('http://abc.com')).toBe(WEBSITE_WRONG_MESSAGE);
-        expect(validateHttpsFunc('https://www')).toBe(WEBSITE_WRONG_MESSAGE);
+        // expect(validateHttpsFunc('https://www')).toBe(WEBSITE_WRONG_MESSAGE);
         expect(validateHttpsFunc('www')).toBe(WEBSITE_WRONG_MESSAGE);
         expect(validateHttpsFunc('.com')).toBe(WEBSITE_WRONG_MESSAGE);
         expect(validateHttpsFunc('дом')).toBe(WEBSITE_WRONG_MESSAGE);
