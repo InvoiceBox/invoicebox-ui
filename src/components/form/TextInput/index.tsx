cbf9e4c1a14ec76ea3127a6e0fadccdc62745042
dmitrii <dmitrii@MacBook-Pro-UnderT.local>
2025-09-25 16:33:31 +0300

    fix ref

--- src/components/form/TextInput/index.tsx
+++ src/components/form/TextInput/index.tsx
@@ -1,4 +1,4 @@
-import React, { ChangeEvent, FC, ReactNode, Ref, useCallback } from 'react';
+import React, { ChangeEvent, ReactNode, useCallback } from 'react';
 import * as S from './styles';
 import { InputLabel, TProps as TInputLabelProps } from '../InputLabel';
 import { PureInput, TProps as TPureInputProps } from '../PureInput';
@@ -27,64 +27,69 @@ export type TProps = Pick<TInputLabelProps, 'label' | 'required'> &
         children?: ReactNode;
         element?: 'input' | 'textarea';
         required?: boolean;
-        ref?: Ref<HTMLInputElement>;
     };
 
-export const TextInput: FC<TProps> = ({
-    hasError = false,
-    onFocus,
-    onBlur,
-    value,
-    onChange,
-    label,
-    name,
-    maxLength,
-    disabled,
-    placeholder,
-    paddingRight,
-    type,
-    children,
-    size = 'M',
-    isOnlyNumbers,
-    autoFocus,
-    element = 'input',
-    rows,
-    required = false,
-    ref,
-}) => {
-    const { inFocus, handleFocus, handleBlur } = useInputFocus({ onFocus, onBlur });
-
-    const handleChange = useCallback(
-        (event: ChangeEvent<HTMLInputElement>) => {
-            onChange(event.target.value.slice(0, maxLength ?? Infinity));
+export const TextInput = React.forwardRef<HTMLInputElement, TProps>(
+    (
+        {
+            hasError = false,
+            onFocus,
+            onBlur,
+            value,
+            onChange,
+            label,
+            name,
+            maxLength,
+            disabled,
+            placeholder,
+            paddingRight,
+            type,
+            children,
+            size = 'M',
+            isOnlyNumbers,
+            autoFocus,
+            element = 'input',
+            rows,
+            required = false,
         },
-        [onChange, maxLength],
-    );
+        ref,
+    ) => {
+        const { inFocus, handleFocus, handleBlur } = useInputFocus({ onFocus, onBlur });
 
-    return (
-        <InputLabel inFocus={inFocus} label={label} disabled={disabled} required={required}>
-            <S.InputLabelContent>
-                <PureInput
-                    ref={ref}
-                    name={name}
-                    disabled={disabled}
-                    hasError={hasError}
-                    inFocus={inFocus}
-                    onFocus={handleFocus}
-                    onBlur={handleBlur}
-                    value={value}
-                    onChange={handleChange}
-                    placeholder={placeholder}
-                    paddingRight={paddingRight}
-                    type={type}
-                    isOnlyNumbers={isOnlyNumbers}
-                    autoFocus={autoFocus}
-                    element={element}
-                    rows={rows}
-                    {...SIZE_PARAMS_MAP[size]}
-                />
-                {children && <S.ChildrenWrapper>{children}</S.ChildrenWrapper>}
-            </S.InputLabelContent>
-        </InputLabel>
-    );
-};
+        const handleChange = useCallback(
+            (event: ChangeEvent<HTMLInputElement>) => {
+                onChange(event.target.value.slice(0, maxLength ?? Infinity));
+            },
+            [onChange, maxLength],
+        );
+
+        return (
+            <InputLabel inFocus={inFocus} label={label} disabled={disabled} required={required}>
+                <S.InputLabelContent>
+                    <PureInput
+                        ref={ref}
+                        name={name}
+                        disabled={disabled}
+                        hasError={hasError}
+                        inFocus={inFocus}
+                        onFocus={handleFocus}
+                        onBlur={handleBlur}
+                        value={value}
+                        onChange={handleChange}
+                        placeholder={placeholder}
+                        paddingRight={paddingRight}
+                        type={type}
+                        isOnlyNumbers={isOnlyNumbers}
+                        autoFocus={autoFocus}
+                        element={element}
+                        rows={rows}
+                        {...SIZE_PARAMS_MAP[size]}
+                    />
+                    {children && <S.ChildrenWrapper>{children}</S.ChildrenWrapper>}
+                </S.InputLabelContent>
+            </InputLabel>
+        );
+    },
+);
+
+TextInput.displayName = 'TextInput';
