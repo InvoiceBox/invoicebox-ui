10c7637ee28509e80563774df6943acf81daf4cf
Dmitry Sorokin <irondimk@gmail.com>
2025-11-14 16:49:22 +0700

    Cross24Icon

---
+++ src/components/form/DateTimeInput/components/Cross24Icon/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export const Cross24Icon = () => {
+    return (
+        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path
+                d="M17 7L7 17M17 17L7 7"
+                stroke="#353F47"
+                strokeWidth="1.5"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+            />
+        </svg>
+    );
+};
