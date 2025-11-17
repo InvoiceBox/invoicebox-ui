82677806230daa83c89cb1edf35fda36bab2c4bf
dmitrii <dima.belik.011100@gmail.com>
2025-11-17 15:15:12 +0300

    change dropdown width

--- src/components/common/Dropdown/styles.ts
+++ src/components/common/Dropdown/styles.ts
@@ -30,7 +30,7 @@ export const Wrapper = styled.div<{
     $palette: TDropdownPalette;
 }>`
     overflow: hidden;
-    border-radius: 16px;
+    border-radius: 20px;
     box-shadow: 0px 10px 17px ${({ $palette }) => $palette.shadow};
     background: ${({ $palette }) => $palette.bg};
 
