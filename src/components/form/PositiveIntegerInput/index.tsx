42a072e228c54c53507d249585f5a3d7f6345667
Dmitry Sorokin <irondimk@gmail.com>
2026-06-25 18:53:10 +0700

    PositiveIntegerInput modern styles fix

--- src/components/form/PositiveIntegerInput/index.tsx
+++ src/components/form/PositiveIntegerInput/index.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useCallback, useMemo } from 'react';
+import React, { FC, useCallback } from 'react';
 import * as S from './styles';
 import { useInputFocus } from '../../../hooks/useInputFocus';
 import { InputLabel, TProps as TInputLabelProps } from '../InputLabel';
@@ -8,8 +8,8 @@ import { useFilters } from './hooks/useFilters';
 import { useNormalizers } from './hooks/useNormalizers';
 import { useIncrement } from './hooks/useIncrement';
 import { useIncrementDisabledFlags } from './hooks/useIncrementDisabledFlags';
-import { MODERN_STYLE_SIZE_PARAMS_MAP, SIZE_PARAMS_MAP, TSizes } from '../constants';
-import { ModernPlaceholder } from '../ModernPlaceholder';
+import { TSizes } from '../constants';
+import { useInputStyles } from '../_hooks/useInputStyles';
 
 export type TProps = {
     value: number | null;
@@ -40,6 +40,15 @@ export const PositiveIntegerInput: FC<TProps> = ({
 }) => {
     const { inFocus, handleFocus, handleBlur } = useInputFocus({ onFocus, onBlur });
 
+    const { inputLabel, paddingAndVariantOptions, modernPlaceholder } = useInputStyles({
+        isHaveValue: typeof value === 'number',
+        useModernStyles,
+        size,
+        label,
+        inFocus,
+        placeholder,
+    });
+
     const filters = useFilters({ max });
     const { normalizeFrom, normalizeTo } = useNormalizers();
     const { increment, decrement } = useIncrement();
@@ -62,43 +71,10 @@ export const PositiveIntegerInput: FC<TProps> = ({
         onChange(decrement(value));
     }, [decrement, value, onChange]);
 
-    const modernInputPlaceholder = useMemo(() => {
-        return placeholder || label;
-    }, [label, placeholder]);
-    const isModernPlaceholderVisible = useMemo(() => !inFocus && value === null, [inFocus, value]);
-
-    const inputLabel = useMemo(() => {
-        if (useModernStyles) {
-            if (value || inFocus) {
-                return label;
-            } else {
-                return undefined;
-            }
-        } else {
-            return label;
-        }
-    }, [inFocus, label, value, useModernStyles]);
-
-    const paddingOptions = useMemo(() => {
-        if ((value || inFocus) && useModernStyles && inputLabel) {
-            return MODERN_STYLE_SIZE_PARAMS_MAP[size];
-        } else {
-            return SIZE_PARAMS_MAP[size];
-        }
-    }, [value, inFocus, useModernStyles, size, inputLabel]);
-
     return (
         <InputLabel inFocus={inFocus} label={inputLabel} useModernStyles={useModernStyles} size={size}>
             <S.ControlWrapper>
-                {useModernStyles && modernInputPlaceholder && (
-                    <ModernPlaceholder
-                        visible={isModernPlaceholderVisible}
-                        paddingTop={SIZE_PARAMS_MAP[size].paddingTop}
-                        size={size}
-                    >
-                        {modernInputPlaceholder}
-                    </ModernPlaceholder>
-                )}
+                {modernPlaceholder}
                 <PureInput
                     name={name}
                     hasError={hasError}
@@ -109,7 +85,7 @@ export const PositiveIntegerInput: FC<TProps> = ({
                     value={normalizeFrom(value)}
                     onChange={handleChange}
                     paddingRight={upAndDown ? 44 : undefined}
-                    {...paddingOptions}
+                    {...paddingAndVariantOptions}
                     useModernStyles={useModernStyles}
                 />
                 {upAndDown && (
